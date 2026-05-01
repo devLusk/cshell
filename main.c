2445a12f24fc2ab4a64c3048d897dbe71221b844
Lucas S. <lucasvida2005@gmail.com>
2026-05-01 16:15:22 -0300

    add read_line function

--- main.c
+++ main.c
@@ -28,3 +28,45 @@ void cshell_loop()
         free(args);
     } while (status);
 }
+
+#define CSHELL_RL_BUFSIZE 1024
+char *cshell_read_line(void)
+{
+    int bufsize = CSHELL_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer)
+    {
+        fprintf(stderr, "cshell: error allocating memory\n");
+        exit(1);
+    }
+
+    while (1)
+    {
+        c = getchar();
+
+        if (c == EOF || c == '\n')
+        {
+            buffer[position] = '\0';
+            return buffer;
+        }
+        else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += CSHELL_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer)
+            {
+                fprintf(stderr, "cshell: error allocating memory\n");
+                exit(1);
+            }
+        }
+    }
+}
